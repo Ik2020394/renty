7bf702f443a387169e3a41b3b5f00a246bcda829
unknown <ikhlassbahdar90@gmail.com>
2024-11-20 09:16:58 +0100

    secand commit

--- lib/screens/car_options_screen.dart
+++ lib/screens/car_options_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
-import 'car_detail_screen.dart'; // Assurez-vous que l'import est correct
+import 'car_detail_screen.dart';
 
-class CarOptionScreen extends StatelessWidget {
+class CarOptionScreen extends StatefulWidget {
   const CarOptionScreen({super.key});
+
+  @override
+  State<CarOptionScreen> createState() => _CarOptionScreenState();
+}
+
+class _CarOptionScreenState extends State<CarOptionScreen> {
+  String? selectedLocation;
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +37,7 @@ class CarOptionScreen extends StatelessWidget {
         'transmission': 'Automatic',
         'airbags': 'Yes',
       },
-      {
+       {
         'name': 'ford',
         'image': '../../assets/sven.jpg',
         'price': 120.0,
@@ -82,74 +89,176 @@ class CarOptionScreen extends StatelessWidget {
         title: const Text('Car Options'),
         backgroundColor: Colors.blueGrey,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: GridView.builder(
-          itemCount: cars.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10.0,
-            mainAxisSpacing: 10.0,
-            childAspectRatio: 0.7,
-          ),
-          itemBuilder: (context, index) {
-            final car = cars[index];
-            return GestureDetector(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.history),
+              title: const Text('Historique'),
+              
               onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CarDetailScreen(car: car),
-                  ),
+                // Naviguez ou ajoutez une action
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profil'),
+              onTap: () {
+                // Naviguez ou ajoutez une action
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              title: const Text('Favoris'),
+              onTap: () {
+                // Naviguez ou ajoutez une action
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: GestureDetector(
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return SizedBox(
+                      height: 150,
+                      child: Column(
+                        children: [
+                          ListTile(
+                            title: const Text("At the agency"),
+                            onTap: () {
+                              setState(() {
+                                selectedLocation = "At the agency";
+                              });
+                              Navigator.pop(context);
+                            },
+                          ),
+                          ListTile(
+                            title: const Text("At the airport"),
+                            onTap: () {
+                              setState(() {
+                                selectedLocation = "At the airport";
+                              });
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 );
               },
-              child: Card(
-                elevation: 3,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white, // Fond blanc
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                      ),
-                      child: Image.asset(
-                        car['image'],
-                        height: 200,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
+                    Text(
+                      selectedLocation ?? "What's your pick-up location?",
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        car['name'],
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 24, 24, 24),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                        '\$${car['price']} / day',
-                        style: const TextStyle(
-                          color: Color.fromARGB(255, 123, 14, 14),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
+                    const Icon(Icons.arrow_drop_down, color: Colors.grey),
                   ],
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: GridView.builder(
+                itemCount: cars.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                  childAspectRatio: 0.7,
+                ),
+                itemBuilder: (context, index) {
+                  final car = cars[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CarDetailScreen(car: car),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      elevation: 3,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              topRight: Radius.circular(12),
+                            ),
+                            child: Image.asset(
+                              car['image'],
+                              height: 200,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Text(
+                              car['name'],
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 24, 24, 24),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              '\$${car['price']} / day',
+                              style: const TextStyle(
+                                color: Color.fromARGB(255, 123, 14, 14),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
